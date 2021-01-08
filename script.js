e92f92b239572b7e6cc7c7debcb08c54a3cf8819
William Addesso <wsaddesso@gmail.com>
2021-01-08 01:33:26 -0500

    add remove event listener for when a box is marked with a player selection. Created event listeners for the player buttons.

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 // Gameboard module. 
 const gameboard = (function () {
-    let gameSquareIds = ["square0", "square1", "square2", "square3", "square4", 
+    let _gameSquareIds = ["square0", "square1", "square2", "square3", "square4", 
     "square5", "square6", "square7", "square8"];
 
-    let gameSquareDoms = gameSquareIds.map(function(squareId) {
+    // An array of the squares document objects.
+    let gameSquareDoms = _gameSquareIds.map(function(squareId) {
         return document.getElementById(squareId);
     })
 
+    // Makes gameSquareDoms public.
     return {gameSquareDoms};
 })() 
+
+
+
+// Create players in response to player number selection
+let _playerButtons = Array.from(document.getElementsByClassName("playerButton"));
+_playerButtons.forEach(function(btn) {
+    btn.addEventListener("click", function() {
+        console.log(btn.id);
+    });
+}); 
 
 
 
 // Player creator factory function.
 const player = (mark) => {
     return {mark};
-}
+} 
 
 let test = player("testMark");
 
@@ -23,12 +35,15 @@ let test = player("testMark");
 // Adds event listener to elements in argument.
 function addEventsToSquares(squares) {
     squares.forEach(function(square) {
-        square.addEventListener("click", function() {
+        square.addEventListener("click", function addEvent() {
             square.innerHTML = test.mark;
             console.log(square);
+            // Remove the event listener function after being clicked.
+            square.removeEventListener("click", addEvent)
         })
     })
 }
+
 
 // Calls function to add event listener to squares.
 addEventsToSquares(gameboard.gameSquareDoms);
