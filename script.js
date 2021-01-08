e14386276241a66b8ceb1f1e3ab614074e171025
William Addesso <wsaddesso@gmail.com>
2021-01-07 20:18:25 -0500

    Created a gameboard module. Created a player factory function. Created a test player. Created function to add events to each square on the board to change the inner HTML.

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-// A module for the gameBoard
-const gameboard = () => {
-    gameboardArr = []
-    return {gameboardArr}
+// Gameboard module. 
+const gameboard = (function () {
+    let gameSquareIds = ["square0", "square1", "square2", "square3", "square4", 
+    "square5", "square6", "square7", "square8"];
+
+    let gameSquareDoms = gameSquareIds.map(function(squareId) {
+        return document.getElementById(squareId);
+    })
+
+    return {gameSquareDoms};
+})() 
+
+
+
+// Player creator factory function.
+const player = (mark) => {
+    return {mark};
 }
 
-const player = (name, symbol) => {
-    return {name, symbol};
+let test = player("testMark");
+
+
+// Adds event listener to elements in argument.
+function addEventsToSquares(squares) {
+    squares.forEach(function(square) {
+        square.addEventListener("click", function() {
+            square.innerHTML = test.mark;
+            console.log(square);
+        })
+    })
 }
+
+// Calls function to add event listener to squares.
+addEventsToSquares(gameboard.gameSquareDoms);
