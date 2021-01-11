850fbe9f36846bed7e0b63f27320d7b86a331460
William Addesso <wsaddesso@gmail.com>
2021-01-11 01:53:18 -0500

    The choose players function returns the players array, which includes either a single player, or two players.

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+// Game functionality module.
+const game = (function() {
+    // Creates players based on which player button is clicked. 
+    const choosePlayers = function() {
+        let onePlayer = document.getElementById("onePlayer");
+        let twoPlayer = document.getElementById("twoPlayer");
+        onePlayer.addEventListener("click", function() {
+            players = [];
+            let xPlayer = Object.create(createPlayer("xPlayer", "x"));
+            players.push(xPlayer);
+            return players;
+        }) 
+        twoPlayer.addEventListener("click", function() {
+            players = [];
+            let xPlayer = Object.create(createPlayer("xPlayer", "x"));
+            let oPlayer = Object.create(createPlayer("oPlayer", "O"));
+            players.push(xPlayer, oPlayer);
+            return players;
+        })
+    }
+
+
+    return choosePlayers();
+
+})()
+
+function currentTurn(playersArr) {
+    if (playersArr.length === 1) {
+        return playersArr[0].mark;
+    }
+}
+
+ 
+// Player creator factory function.
+const createPlayer = (name, mark) => {
+    return {name, mark};
+}
+
+
+
+
 // Gameboard module. 
 const gameboard = (function () {
     let _gameSquareIds = ["square0", "square1", "square2", "square3", "square4", 
@@ -12,7 +53,7 @@ const gameboard = (function () {
     function addEventsToSquares(squares) {
         squares.forEach(function(square) {
             square.addEventListener("click", function addEvent() {
-                square.innerHTML = test.mark;
+                square.innerHTML = "test";
                 console.log(square);
                 // Remove the event listener function after being clicked.
                 square.removeEventListener("click", addEvent)
@@ -23,39 +64,7 @@ const gameboard = (function () {
 
     // Calls function to add event listener to squares.
     return addEventsToSquares(gameSquareDoms);
-})() 
+})()  
 
 
-
-
-
-// Player creator factory function.
-const createPlayer = (mark) => {
-    return {mark};
-}
-
-let test = Object.create(createPlayer("M"));
-
-
-
-
-// Game functionality method.
-const game = (function() {
-    // Creates players based on which player button is clicked. 
-    function choosePlayers() {
-        let onePlayer = document.getElementById("onePlayer");
-        let twoPlayer = document.getElementById("twoPlayer");
-        onePlayer.addEventListener("click", function() {
-            let xPlayer = Object.create(createPlayer("x"));
-            console.log(xPlayer.mark);
-        })
-        twoPlayer.addEventListener("click", function() {
-            let xPlayer = Object.create(createPlayer("EX"));
-            let oPlayer = Object.create(createPlayer("O"));
-            console.log(xPlayer.mark, oPlayer.mark); 
-        })
-    }
-
-    return choosePlayers();
-
-})()
+//console.log(currentTurn(players));
