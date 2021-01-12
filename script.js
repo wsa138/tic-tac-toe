57d2fe01290032aaf4bb0e4828a6eb560ae52906
William Addesso <wsaddesso@gmail.com>
2021-01-11 23:56:19 -0500

    Removed some code in the script to clean up the code and use the modules in a different way. Changed html display text.

--- script.js
+++ script.js
@@ -1,70 +1,44 @@
-// Game functionality module.
-const game = (function() {
-    // Creates players based on which player button is clicked. 
-    const choosePlayers = function() {
-        let onePlayer = document.getElementById("onePlayer");
-        let twoPlayer = document.getElementById("twoPlayer");
-        onePlayer.addEventListener("click", function() {
-            players = [];
-            let xPlayer = Object.create(createPlayer("xPlayer", "x"));
-            players.push(xPlayer);
-            return players;
-        }) 
-        twoPlayer.addEventListener("click", function() {
-            players = [];
-            let xPlayer = Object.create(createPlayer("xPlayer", "x"));
-            let oPlayer = Object.create(createPlayer("oPlayer", "O"));
-            players.push(xPlayer, oPlayer);
-            return players;
-        })
+
+// Gameboard module.
+const gameBoard = (function() {
+    // Creates an array of each square element.
+    let gameBoardArray = Array.from(document.getElementsByClassName("gameSquare"));
+
+    // Example function
+    function test(i) {
+        console.log(i);
     }
 
+    return {
+        gameBoardArray,
+        test
+    }
+})();
 
-    return choosePlayers();
 
-})()
+// Contains all functions that alter the display.
+const displayController = function() {
+    // A function that sets event listeners on squares and runs the markSquare function.
+    const setAvailableSquares = function(arr) {
+        console.log(arr)
+    }
 
-function currentTurn(playersArr) {
-    if (playersArr.length === 1) {
-        return playersArr[0].mark;
+    return {
+        setAvaileableSquare: setAvailableSquares
     }
 }
 
- 
-// Player creator factory function.
-const createPlayer = (name, mark) => {
-    return {name, mark};
-}
 
 
-
-
-// Gameboard module. 
-const gameboard = (function () {
-    let _gameSquareIds = ["square0", "square1", "square2", "square3", "square4", 
-    "square5", "square6", "square7", "square8"];
-
-    // An array of the squares document objects.
-    let gameSquareDoms = _gameSquareIds.map(function(squareId) {
-        return document.getElementById(squareId);
-    });
-
-    // Adds event listener to elements in argument.
-    function addEventsToSquares(squares) {
-        squares.forEach(function(square) {
-            square.addEventListener("click", function addEvent() {
-                square.innerHTML = "test";
-                console.log(square);
-                // Remove the event listener function after being clicked.
-                square.removeEventListener("click", addEvent)
-            })
-        })
+// Factory creates a player object with a name and their mark(x or o).
+const player = function(name, mark) {
+    return {
+        name,
+        mark
     };
+}
 
 
-    // Calls function to add event listener to squares.
-    return addEventsToSquares(gameSquareDoms);
-})()  
 
-
-//console.log(currentTurn(players));
+/* An object that controls the flow of the game. */
+const gameflow = {}
